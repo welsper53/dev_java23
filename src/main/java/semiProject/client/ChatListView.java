45c14d9d5c89cb3c9e6ff14ee8129753e09eb16e
welsper53 <welsper53@gmail.com>
2023-01-03 19:53:58 +0900

    2023-01-03 세미프로젝트 출력리스트 수정

--- src/main/java/semiProject/client/ChatListView.java
+++ src/main/java/semiProject/client/ChatListView.java
@@ -4,97 +4,74 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import java.util.Vector;
 
 public class ChatListView extends JFrame implements ActionListener {
+    // "ChatListViewDiagram" 호출
+    ChatListViewDialog clvDiagram = new ChatListViewDialog(this);
+
     // [선언부]
     Vector<JButton> vList = new Vector<JButton>();
 
-    JPanel jp_north = new JPanel();
+    // NORTH
     JButton jbtn_myPage = new JButton("마이페이지");
     JButton jbtn_firChan = new JButton("새 채팅");   // 친구추가 | 새 채팅
 
-
-    JPanel jp_center = new JPanel();
+    // CENTER
     JLabel jlb_secChan = new JLabel("채팅 목록");    // 친구 | 채팅 목록
     JPanel jp_secChan = new JPanel();   // TODO: 중앙 패널 <- 리스트
-    JLabel jlb_test = new JLabel("테스트용");
-    JButton jbtn_cenList = null;    // TODO: 중앙 - 친구, 채팅 리스트
-//    JScrollPane jsp = new JScrollPane();    // TODO: 리스트를 스크롤
-//    JScrollPane jsp_center = new  // TODO: 스크롤
+    JButton jbtn_list = null;    // TODO: 친구 | 채팅 리스트
+    JScrollPane jsp_display = new JScrollPane(jp_secChan, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);    // TODO: 리스트를 스크롤
 
+    // SOUTH
     JPanel jp_south = new JPanel();
     JButton jbtn_main = new JButton("내 화면");
     JButton jbtn_chat = new JButton("채팅방");
 
 
     public ChatListView() {
-        for (int i=0; i<3; i++) {
-            jbtn_cenList = new JButton(Integer.toString(i));
-            vList.add(jbtn_cenList);
+        for (int i=0; i<20; i++) {
+            jbtn_list = new JButton(Integer.toString(i));
+            vList.add(jbtn_list);
         }
-    }
+    } // end of ChatListView()
 
     public void initDisplay() {
-    /*
-        // 북
-        jbtn_myPage.setBounds(10, 10, 150, 100);
-        jbtn_firChan.setBounds(200, 10, 150, 100);
-//        jbtn_myPage.setSize(150, 100);
-//        jbtn_firChan.setSize(150, 100);
-
-//        jp_north.setLayout(new GridLayout(1,2));
-        jp_north.add(jbtn_myPage);
-        jp_north.add(jbtn_firChan);
-
-        // 중
-//        jp_center.setLayout(new GridLayout(2,1));
-        jp_center.add(jlb_secChan);
-        jp_secChan.add(jbtn_cenList);
-        jp_center.add(jp_secChan);
-
-        // 남
-//        jp_south.setLayout(new GridLayout(1,2));
-        jp_south.add(jbtn_main);
-        jp_south.add(jbtn_chat);
-
-        this.add(jp_north);
-        this.add(jp_center);
-        this.add(jp_south);
-
-
-        this.add("North", jp_north);
-        this.add("Center", jp_center);
-        this.add("South", jp_south);
-
-     */
         this.setLayout(null);
 
-        // 북
+        // [north]
         jbtn_myPage.addActionListener(this);
         jbtn_myPage.setBounds(10, 10, 150, 40);
         this.add(jbtn_myPage);
-        jbtn_firChan.addActionListener(this);
+        jbtn_firChan.addActionListener(clvDiagram);
         jbtn_firChan.setBounds(175, 10, 150, 40);
         this.add(jbtn_firChan);
 
 
-        // 중
+        // [center]
         jlb_secChan.setBounds(10, 60, 330, 20);
         this.add(jlb_secChan);
-        jp_secChan.setBounds(5, 85, 340, 420);
-        jp_secChan.setLayout(new BorderLayout());
-//        jp_secChan.add(jlb_test);
+        // 중앙 리스트 출력, TODO: 스크롤바로 나올 수 있게 해야함
+//        jp_secChan.setBounds(5, 85, 330, 420);
+        jsp_display.setBounds(5, 85, 325, 420);
+        jp_secChan.setLayout(new GridLayout(vList.size(), 1));
+
+        // 버튼을 벡터에 삽입
         for (int i=0; i<vList.size(); i++) {
-            vList.get(i).setSize(300, 50);
             jp_secChan.add(vList.get(i));
             vList.get(i).addActionListener(this);
         }
 
-        this.add(jp_secChan);
+//        this.add(jp_secChan);
+        this.add(jsp_display);
 
 
-        // 남
+        // [south]
         jbtn_main.addActionListener(this);
         jbtn_main.setBounds(10, 510, 150, 40);
         this.add(jbtn_main);
@@ -102,12 +79,20 @@ public class ChatListView extends JFrame implements ActionListener {
         jbtn_chat.setBounds(175, 510, 150, 40);
         this.add(jbtn_chat);
 
+        // 창 위치 지정
         this.setLocation(500, 100);
+        // 창 크기 설정
         this.setSize(350, 600);
         this.setTitle("채팅 목록");
         this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+
+        this.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e) {
+                System.out.println("종료합니다.");
+                System.exit(0);
+            }
+        });
+    } // end of initDisplay()
 
     public static void main(String[] args) {
         ChatListView cView = new ChatListView();
@@ -116,7 +101,7 @@ public class ChatListView extends JFrame implements ActionListener {
         System.out.println("화면 출력");
         cView.initDisplay();
 
-    }
+    } // end of main()
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -126,23 +111,27 @@ public class ChatListView extends JFrame implements ActionListener {
             // 마이페이지 클릭
             System.out.println("jbtn_myPage(마이페이지클릭) 클릭");
         }
-        else if(obj == jbtn_firChan) {
-            // 친추/새채팅 클릭
-            System.out.println("jbtn_firChan(" + jbtn_firChan.getText() +") 클릭");
-        }
         else if (obj == jbtn_main) {
             // 친구목록 클릭
             System.out.println("jbtn_myPage(내 화면) 클릭");
-            this.setTitle("친구목록");
+            this.setTitle("친구 목록");
             jbtn_firChan.setText("친구추가");
             jlb_secChan.setText("친구 목록");
         }
         else if (obj == jbtn_chat) {
             // 채팅목록 클릭
             System.out.println("jbtn_chat(채팅방) 클릭");
-            this.setTitle("채팅목록");
+            this.setTitle("채팅 목록");
             jbtn_firChan.setText("새 채팅");
             jlb_secChan.setText("채팅 목록");
         }
-    }
+
+
+        // 목록버튼 클릭
+        for (int i=0; i<vList.size(); i++) {
+            if (obj == vList.get(i)) {
+                System.out.println("jbtn_list(" + vList.get(i).getText() +") 클릭");
+            }
+        }
+    } // end of actionPerformed()
 }
